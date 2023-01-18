1770d562b890d6489ca0f066ae3b5d1b73a0d587
joseph cavaretta <josephmc241@gmail.com>
2023-01-18 06:39:26 -0700

    edited out env variable:

--- weather_model/scripts/run.sh
+++ weather_model/scripts/run.sh
@@ -3,5 +3,4 @@ docker run \
 --rm \
 --volume ~/projects/weather_model/src/data:/data \
 --volume ~/projects/weather_model/src/data/scheduled_runs:/output \
--e GOOGLE_APPLICATION_CREDENTIALS=/run/user/1000/gvfs/google-drive:host=gmail.com,user=josephmc241/0AMIJ0BdVNPMaUk9PVA/10mVXLWqGBfgPcuY3qD_GbwLdcGyMhy8O/1tAbHoHj-y3-M_s8rRSXgbLmfBgxVZ2cE \
 weather_model
