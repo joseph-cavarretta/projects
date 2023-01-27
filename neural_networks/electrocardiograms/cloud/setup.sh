f1a14639fb1bccb628b3f92c33cb1da2b4e92740
joseph cavaretta <josephmc241@gmail.com>
2023-01-27 13:37:36 -0700

    added pyspark neural network for EKGs

---
+++ neural_networks/electrocardiograms/cloud/setup.sh
@@ -0,0 +1,24 @@
+# spark ui: spark://ubuntu:7077
+
+# start standalone master server
+start-master.sh
+
+# listening on port 8080
+sudo ss -tunelp | grep 8080
+
+# start slave worker process
+start-slave.sh spark://ubuntu:7077
+
+# if script not in PATH, use this to locate it and run with full path
+sudo updatedb
+locate start-slave.sh
+
+# start spark shell
+/opt/spark/bin/spark-shell
+
+# OR start pyspark shell
+$ /opt/spark/bin/pyspark
+
+# shut down processes
+SPARK_HOME/sbin/stop-slave.sh
+SPARK_HOME/sbin/stop-master.sh
